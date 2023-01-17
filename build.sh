acab192443dac4e999175cf801ce8fc483f54a3f
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2023-01-16 23:17:19 -0300

    fix: Exit on error.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build build -j`getconf _NPROCESSORS_ONLN`
 
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # install vcpkg
 git clone https://github.com/Microsoft/vcpkg.git
 ./vcpkg/bootstrap-vcpkg.sh --disableMetrics

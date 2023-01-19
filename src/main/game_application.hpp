692b52763897c01ad52c549ccf3c38995d55df4d
Leonardo <leonardodicredico@gmail.com>
2023-01-19 18:01:19 -0300

    Feat: GameApplication class

---
+++ src/main/game_application.hpp
@@ -0,0 +1,18 @@
+
+//local
+#include "../graphics/graphic_manager.hpp"
+
+class GameApplication
+{
+private:
+    bool shall_quit = false;
+
+public:
+    GraphicManager graphic_manager{boarglib::Vector2i32{1280,720}};
+
+    void run();
+
+private:
+    void handle_input();
+
+};
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,42 +3,17 @@
 // #include <foobar.hpp>
 
 
+//builtin
 #include <iostream>
 
+//third party
 #include <SDL.h>
 
-#include <graphics/graphic_manager.hpp>
-
+//local
+#include "./game_application.hpp"
 
 int main() {
 
-    std::cout << "rimboar" << std::endl;
-    GraphicManager graphical_manager{boarglib::Vector2i32{1280,720}};
-
-    SDL_Event event;
-    bool shall_quit = false;
-    while(!shall_quit)
-    {
-
-        while(SDL_PollEvent(&event))
-        {
-            if(event.type == SDL_QUIT)
-                shall_quit = true;
-            else if(event.type == SDL_KEYDOWN)
-            {
-                switch (event.key.keysym.sym)
-                {
-                    case SDLK_ESCAPE:
-                        shall_quit = true;
-                        continue;
-                    
-                    default:
-                        continue;
-                }
-
-            }
-        }
-        
-        graphical_manager.render();
-    }
+    GameApplication rimboar{};
+    rimboar.run();
 }

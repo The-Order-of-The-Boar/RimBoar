02ef31fda435802bc274298633a7cc1a1a2d9776
Leonardo <leonardodicredico@gmail.com>
2023-02-23 11:44:08 -0300

    Chore: Made benchmark use Relase build mode

--- benchmark.sh
+++ benchmark.sh
@@ -3,7 +3,7 @@ set -e
 
 source scripts/any_newer_than.sh
 
-test_build_type="Debug"
+test_build_type="Release"
 
 if !([ -e "build" ]) || [ $test_build_type != "$(cat build/BUILD_TYPE)" ] || (any_newer_than build/test_rimboar src CMakeLists.txt build.sh test.sh); then
     ./build.sh $test_build_type tests
